5acfb1b6e0bc7812f4a5afc9ebe5868eb4150974
Max <vloxomire@gmail.com>
2018-09-17 00:52:29 -0300

    0052

---
+++ app/src/main/java/com/max/poyecto1/vistasAvanzadas.java
@@ -0,0 +1,13 @@
+package com.max.poyecto1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vistasAvanzadas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_vistas_avanzadas);
+    }
+}
